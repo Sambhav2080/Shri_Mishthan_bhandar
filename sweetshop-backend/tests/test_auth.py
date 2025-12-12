65945d1232f9468be96c3b387c53c0ece04ac4e7
Sambhav Kapoor <sambhavoct2004@gmail.com>
2025-12-13 02:22:37 +0530

    testing: add failing user registration test case

---
+++ sweetshop-backend/tests/test_auth.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+def test_register_user():
+    #assigning payload
+    payload = {
+        "name":"Sambhav",
+        "email":"sambhavoct2004@gmail.com",
+        "pasward":"pasward123"
+    }
+    #creating a post request
+    response = client.post("/api/auth/register",json=payload)
+
+    #results expected
+    assert response.status_code ==201
+    data = response.json()
+
+    #return valid fields
+    assert data["name"] =="Sambhav"
+    assert data["email"] =="sambhavoct2004@gmail.com"
+    assert "id" in data
